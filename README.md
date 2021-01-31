685b69c86064518ba9aeb48c40de4563affa74a9
lightlySkewed <michael.v.cassidy@gmail.com>
2021-01-31 14:17:25 -0500

    Updated to latest version/ files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# FOMC_Sentiment_Macro_Data
-Code created to properly format macro and market data for regression analysis.
+# FOMC-Shock-Macro-Variables
+Contains code and files to generate multi-horizon views of RGDP and PCE
+
+This project was developed to format and combine; historical vintage data from the Federal Reserve Bank of St. Louis's ALFRED database, and forecast data from the Federal Reserve Bank of Philadelphia's Survey of Professional Forecaster (SPF) database.
+
+Specifically, the script (and helper functions) ingests ALFRED xls files and SPF xlsx files, calculates appropriate percent changes (where necessary) and combines the data into a single dataframe or optional csv file.
+
+Developed using: 
+Python 3.8.5
+Jupyter Notebook
+
+Dependencies:
+Pandas
+ipdb
+iGetByWithALittle
+
+NOTES:
+
+RGDP or GDP: This is U.S. Real (inflation-adjusted) Gross Domestic Product (RGDP), a measure of growth for the United States economy. It can be thought of as the sum of all goods sold at a certain price over a period. In this dataset it is expressed in percent change for stationarity purposes.
+
+PCE: This is the U.S. Core Personal Consumption Expenditure Price Index. It is a measure of inflation sans food and energy prices, which are removed to reduce unnecessary variance from the time series. This data only has forecasts starting in 2007. As such, it is not included in the final output, but may be in the future.
+
+PGDP/ GDP Price Index: This is the GDP price index. While it captures price inflation in a manner similar to PCE (above), it differs in two primary ways for the purpose of this research. 1) This index includes not only food and energy prices, but all prices which contribute to U.S. GDP -- making it potentially more volatile. 2) It is included as a variable in the SPF survey from the beginning, and therefor offers us a much deeper history.
+
+Miles: See the last frame of the notebook. I built a single df (named 'macro_data') that contains only the growth (gdp) and inflation (price index) data that we want to use for the initial regressions. Hope it helps.
--- iGetByWithALittle.py
+++ iGetByWithALittle.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import datetime as dt
+
 #define helper functions for FOMC Paper Project
 def find_target_columns(dframe, target_start_char, target_end_char, target):
     '''returns a list of columns containing a target in the column name'''
@@ -38,7 +41,140 @@ def find_new_obs_percent_chg(dframe, starting_column, annualize_pct_chg=0):
             results.append(quarterly_percent_change)
         
     return results
+
+def find_new_vintage_percent_chg(dframe, FOMC_date_list, column_A_name='lag', column_B_name='coincident', annualize_pct_chg=0):
+    '''returns a data frame of vintage lagged and coincident percent change values from vintage levels'''
+    '''this code takes forever to run. it needs to be refactored.'''
+    # create a dictionary to store the conincident vintages' percent changes
+    coincident_results = {}
     
+    # create a dictionary to store the previous vintages' percent changes
+    previous_results = {}
+    
+    # create a dataframe to store and return the results
+    rDf = pd.DataFrame(index=FOMC_date_list)
+    
+    # remove benchmark revisions where last observation is left uncalculated
+    # for example, see GDPC1 from FRED, 20031210 vintage, last obs ~07/01/2003 
+    for col in dframe:
+        i = dframe.columns.get_loc(col)
+        if i > 0:
+            col_last_index = dframe.iloc[:, i].last_valid_index()
+            prev_col_last_index = dframe.iloc[:, i - 1].last_valid_index()
+            if col_last_index < prev_col_last_index:
+                dframe.drop(columns=col, inplace = True)
+
+        # export dframe to csv
+    dframe.to_csv('dframe.csv')
+    
+    # create a list to store associated column dates 
+    temp_coin_column_dates_list = []
+    
+    # for each FOMC meeting
+    for meet in FOMC_date_list:
+        
+        # clear list after previous iteration
+        temp_coin_column_dates_list.clear()
+        
+        # create a list to store associated column dates for the second loop
+        temp_prev_column_dates_list = []
+        
+        # capture the FRED column prefix including '_'
+        FRED_prefix = dframe.columns[-1][:len(dframe.columns[-1])-len(dframe.columns[-1][-8:])]
+        
+        # find the vintages before the current meeting
+        for col in dframe.columns:
+            
+            # capture the date of the column being referenced as a datetime
+            col_date = dt.datetime.strptime(col[-8:], '%Y%m%d')
+
+            # # if the current column's date is before the meeting's date
+            if col_date < meet:
+
+                # # add the column date to a temporary list of column dates
+                temp_coin_column_dates_list.append(col_date)
+
+        # find the largest date in the temporary list. this should be the date of the vintage that immediately preceeds the meeting being referenced
+        coincident_vintage = max(temp_coin_column_dates_list)
+        
+        # construct the name of the appropriate FRED data column
+        coincident_col_ref = FRED_prefix + dt.datetime.strftime(coincident_vintage, '%Y%m%d')
+        
+        # find the date index of the last non-missing 
+        last_coincident_index = dframe[coincident_col_ref].last_valid_index()
+        
+        # find the last value of the data preceeding the meeting
+        last_coincident_value = dframe[coincident_col_ref].loc[last_coincident_index]
+        
+        # find the date index's index integer value 
+        last_coincident_index_int = dframe.index.get_loc(last_coincident_index)
+        
+        # find the preceeding observation's int index value
+        prev_coincident_index_int = last_coincident_index_int - 1
+        
+        # find the second to last value of the data immediately preceeding the meeting
+        prev_coincident_value = dframe[coincident_col_ref].iloc[prev_coincident_index_int]
+        
+        # calculate the percent change in the variable immediately preceeding the meeting
+        coincident_percent_change = (last_coincident_value - prev_coincident_value)/prev_coincident_value*100
+        
+        # append meeting and coincident percent change to coincident_results dict
+        coincident_results[meet] = coincident_percent_change
+        
+        # for each column in the dataframe
+        for col in dframe.columns:
+            
+            # find the last valid index of the current column
+            int_loc_of_columns_last_index = dframe.index.get_loc(dframe[col].last_valid_index())
+            
+            # if the last valid integer index of the current column is equal to one less than the 
+            # last valid integer index of the coincident column, then
+            if int_loc_of_columns_last_index == last_coincident_index_int - 1:
+                
+                # capture the date of the column being referenced as a datetime
+                col_date = dt.datetime.strptime(col[-8:], '%Y%m%d')
+            
+                # append the date to a temporary list
+                temp_prev_column_dates_list.append(col_date)
+                
+        # find the maximum date of this list
+        previous_vintage = max(temp_prev_column_dates_list)
+        
+        # construct the name of the appropriate previous FRED data column
+        previous_col_ref = FRED_prefix + dt.datetime.strftime(previous_vintage, '%Y%m%d')
+        
+        # find the last valid index of the appropriate previous FRED data column
+        last_previous_index = dframe[previous_col_ref].last_valid_index()
+        
+        # find the last valid integer index of the appropriate previous FRED data column
+        last_previous_index_int = dframe.index.get_loc(last_previous_index)
+        
+        # find the second to last valid integer index of the appropriate previous FRED data column
+        prev_previous_index_int = last_previous_index_int - 1
+
+        # find the last value of the previous data preceeding the meeting
+        last_previous_value = dframe[previous_col_ref].loc[last_previous_index]
+        
+        # grab the second-to-last value of the column referenced above
+        prev_previous_value = dframe[previous_col_ref].iloc[prev_previous_index_int]
+        
+        # calculate the percent change in the values
+        prev_percent_change = (last_previous_value - prev_previous_value)/prev_previous_value*100
+
+        # save the value to the 'previous' dict
+        previous_results[meet] = prev_percent_change
+
+        # print("Meeting Date: ", meet)
+        # print("Coincident Percent Change: ", coincident_percent_change)
+        # print("Previous Percent Change: ", prev_percent_change)
+
+    # combine results into a single dataframe
+    results = pd.DataFrame({column_A_name:pd.Series(previous_results),column_B_name:pd.Series(coincident_results)})
+    
+    # return dataframe
+    return results
+
+
 # find index of any dict item in EPU_dict
 def find_index_in_Excel_File_dict(dic):
     for key in dic.keys():
